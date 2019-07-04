b6e91296c18d07da17c85ab358262a091f3546ef
ITSUPPORT <ITSUPPORT@MDTR02.mlad.in>
2019-07-04 17:21:36 +0530

    committed

---
+++ maven-project/src/main/java/com/selenium/test/maven_project/Maven1.java
@@ -0,0 +1,18 @@
+package com.selenium.test.maven_project;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Maven1 
+{
+	public static void main(String[] args) 
+	{
+		
+		
+		System.setProperty("webdriver.chrome.driver","D:/driver/New folder/chromedriver.exe");
+		WebDriver d=new ChromeDriver();
+		d.get("https://connect.maveric-systems.com/index.php/site/login");
+		d.quit();
+		
+	}
+}
